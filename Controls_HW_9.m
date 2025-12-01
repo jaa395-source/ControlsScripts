c0a96f665ed881126b28d2ea591327660aa49d8f
jaa395-source <jaa395@cornell.edu>
2025-11-30 19:13:47 -0500

    yup

--- Controls_HW_9.m
+++ Controls_HW_9.m
@@ -33,18 +33,61 @@ for j = 1:length(k_desired_sets)
 
     k_r = -1/(Ct*inv(At)*Bt);
     final_tf = k_r*new_sys;
-    z.("poles_" + string(j)) = initial(feedback(ss(A-B*K-L*C,L,K,0)*ss(A,B,C,D), 1), [1;0;0;0]);
+    z.("poles_" + string(j)) = initial(feedback(ss(A,B,C,D)*ss(A-B*K-L*C,L,K,0), 1), [1;0;0;0]); 
+
     u.("poles_" + string(j))= initial(feedback(ss(A-B*K-L*C,L,K,0),ss(A,B,C,D)), [1;0;0;0]);
     bode(ss(A-B*K-L*C,L,K,0)*ss(A,B,C,D));
-    [Gm_ss, PM_ss] = margin(ss_tf);
+    [Gm_ss, PM_ss] = margin(final_tf);
     legend_string = horzcat(legend_string, "Poles @ [" + string(curr_poles(1)) + ", " + string(curr_poles(2)) + "], GM = " + string(Gm_ss) + ", PM = " + string(PM_ss));
 end
 legend(legend_string);
 hold off;
 
+%% 
 struct_fields = string(fieldnames(z));
 
-z_fig
+z_fig = figure();
+hold on;
+title("Response to Initial Conditions");
+xlabel("Time (seconds)");
+ylabel("Amplitude");
+
 u_fig = figure();
+hold on;
+title("Response to Initial Conditions");
+xlabel("Time (seconds)");
+ylabel("Amplitude");
+
+legend_string = {};
+
+for field = 1:length(struct_fields)
+    figure(z_fig);
+    plot(z.(struct_fields(field)))
+
+    figure(u_fig);
+    plot(u.(struct_fields(field)))
 
 
+    legend_string = horzcat(legend_string, "Poles @ [" + string(k_desired_sets(field, 1)) + ", " + string(k_desired_sets(field, 2)) + "]");
+end
+
+figure(z_fig);
+legend(legend_string);
+
+figure(u_fig);
+legend(legend_string);
+
+%% Problem 2
+
+%2a
+v0 = 5;
+[M, C, K0, K2] = matricies();
+[a_mat,b_mat,c_mat,d_mat] = generate_state_space_matricies(M, C, K0, K2, v0);
+Q = [0 0 0 0;
+     0 1 0 0;
+     0 0 0 0;
+     0 0 0 0];
+sys = ss(a_mat, b_mat, c_mat, d_mat);
+R = 1;
+[K,S,P] = lqr(sys,Q,R);
+eig(a_mat - b_mat*K)
